38299224fad3ceeab26187827533f617120d0f32
Heedo Kim <devheedoo@gmail.com>
2024-05-18 03:22:00 +0900

    feat(to-dos): 카드 컴포넌트 적용 및 레이아웃 수정

--- apps/to-dos/app/page.tsx
+++ apps/to-dos/app/page.tsx
@@ -1,30 +1,36 @@
 import Card from "./components/Card";
 import List from "./components/List";
 
+import { CARD_ITEMS } from "./fake-data/cardData";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen">
-      <aside className="flex min-h-screen w-24 justify-center bg-[#1C1D22] p-4">
+    <main className="flex min-h-screen w-screen">
+      <aside className="flex min-h-screen w-24 shrink-0 justify-center bg-[#1C1D22] p-4">
         <h1 className="leading-none text-white">TODO</h1>
       </aside>
 
-      <nav className="flex min-h-screen w-[320px] bg-[#222327] p-6">
+      <nav className="flex min-h-screen w-[320px] shrink-0 bg-[#222327] p-6">
         <h2 className="text-3xl leading-none text-white">Projects</h2>
       </nav>
 
-      <section className="min-h-screen w-full bg-[#2A2B2F]">
-        <header className="flex h-24 items-center border-b border-white/10 p-6">
+      <section className="flex max-h-screen min-w-0 grow flex-col bg-[#2A2B2F]">
+        <header className="flex h-24 shrink-0 items-center border-b border-white/10 p-6">
           <span className="leading-none text-white">
             Welcome back, Heedo 👋
           </span>
         </header>
 
-        <div className="flex gap-x-4">
-          <List>
-            <Card />
-            <Card />
-            <Card />
+        <div className="flex min-h-0 grow gap-x-4 overflow-x-scroll p-4">
+          <List title="To do" itemsCount={CARD_ITEMS.length}>
+            {CARD_ITEMS.map((item) => (
+              <Card key={item.title} {...item} />
+            ))}
           </List>
+
+          <List title="Doing" itemsCount={0}></List>
+
+          <List title="Done" itemsCount={0}></List>
         </div>
       </section>
     </main>
