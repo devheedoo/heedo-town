b85c74eeb9c0417a60a3db9055f71f31e1fe0042
Heedo Kim <devheedoo@gmail.com>
2024-05-31 15:40:19 +0900

    feat: 앱 생성

---
+++ apps/nextjs-boilerplate/src/app/page.tsx
@@ -0,0 +1,14 @@
+import classNames from "classnames";
+
+export default function Home() {
+  return (
+    <div
+      className={classNames(
+        "flex min-h-screen items-center justify-center",
+        "text-3xl font-extrabold uppercase text-green-700"
+      )}
+    >
+      Home
+    </div>
+  );
+}
