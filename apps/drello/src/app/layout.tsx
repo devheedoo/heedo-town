f583088f0d1755f1d37909298943b4fe17885fd7
Heedo Kim <devheedoo@gmail.com>
2024-06-01 15:16:00 +0900

    build: Nx Cloud CI 파일 제거

--- apps/drello/src/app/layout.tsx
+++ apps/drello/src/app/layout.tsx
@@ -6,8 +6,8 @@ import { Noto_Sans_KR } from "next/font/google";
 const notoSansKR = Noto_Sans_KR({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Next.js Boilerplate",
-  description: "Set by Heedo",
+  title: "Drello",
+  description: "Heedo's Trello",
 };
 
 export default function RootLayout({
