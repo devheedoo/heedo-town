4536c2b2f704419b5b34913c58d68ae12dbe4fbc
Heedo Kim <devheedoo@gmail.com>
2024-07-19 20:21:25 +0900

    feat(hodo-list): Initialize with boilerplate

---
+++ apps/hodo-list/README.md
@@ -0,0 +1,3 @@
+# Hodo List
+
+Heedo's Todo List
---
+++ apps/hodo-list/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { Noto_Sans_KR } from "next/font/google";
+
+const notoSansKR = Noto_Sans_KR({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Next.js Boilerplate",
+  description: "Set by Heedo",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body className={notoSansKR.className}>{children}</body>
+    </html>
+  );
+}
