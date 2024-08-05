df15e809365c1edcf58fc03b884312e40c49f1ba
Heedo Kim <devheedoo@gmail.com>
2024-08-05 15:24:22 +0900

    fix(hodo-list): Display minutes (MM -> mm)

--- apps/hodo-list/src/utils/date-format.ts
+++ apps/hodo-list/src/utils/date-format.ts
@@ -2,7 +2,7 @@ import { format, isToday } from "date-fns";
 
 export function showTodayTimeOrDate(d: number) {
   if (isToday(d)) {
-    return format(d, "HH:MM:ss");
+    return format(d, "HH:mm:ss");
   }
   return format(d, "d MMM yyyy");
 }
